ebc9e17ab4bc5f4c3c058359829be4f81808cea1
mariellemar <mariellemarsantos@gmail.com>
2023-08-01 11:04:25 -0400

    updated API - methods

--- drinksApp/urls.py
+++ drinksApp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_detail)
 ]
+
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
